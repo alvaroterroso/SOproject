7a983cc53fa4cd7e3cf55b9af815aa85c882b8c9
Álvaro Terroso <106986189+alvaroterroso@users.noreply.github.com>
2024-03-13 16:09:06 +0000

    Add files via upload

---
+++ mobile_user.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "mobile_user.h"
+
+int main(int argc, char **argv){
+	if(argc < 7){
+		printf("Wrong input command!\nUsage: mobile_user <plafond inicial> "
+		       "<número de pedidos de autorização> <intervalo VIDEO> "
+		       "<intervalo MUSIC> <intervalo SOCIAL> <dados a reservar>\n");
+		exit(1);
+	}
+	mobile_user_struct *mobile_user = malloc(sizeof(mobile_user_struct));
+	for(int i = 1; i < 7; i++){
+		if(argv[i] < 0){
+			printf("INPUT ERROR!\n%d must be < 0", atoi(argv[i]));
+			exit(1);
+		}
+	}
+	
+	mobile_user->init_plafond = atoi(argv[1]);
+	mobile_user->auth_request_number =atoi(argv[2]);
+	mobile_user->video_interval = atoi(argv[3]);
+	mobile_user->music_interval = atoi(argv[4]);
+	mobile_user->social_interval = atoi(argv[5]);
+	mobile_user->to_reserve_data = atoi(argv[6]);
+
+	return 0;
+}
---
+++ shared_mem.h
@@ -0,0 +1,53 @@
+//Álvaro Terroso 2021213782
+//Rui Oliveira 2022210616
+
+#ifndef shared_mem   /* Include guard */
+#define shared_mem
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <semaphore.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <sys/msg.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <sys/time.h>
+
+#define PIPENAME_1 "USER_PIPE"
+
+#define PIPENAME_2 "BACK_PIPE"
+
+#define MAX_STRING_SIZE 256
+
+typedef struct config_struct{
+	int queue_slot_number;
+	int max_auth_servers;
+	int auth_proc_time; //ms
+	int max_video_wait; //ms
+	int max_others_wait; //ms
+}config_struct;
+
+typedef struct mobile_user_struct{
+	int init_plafond;
+	int auth_request_number;
+	int video_interval;
+	int music_interval;
+	int social_interval;
+	int to_reserve_data;
+}mobile_user_struct;
+
+//inicializar shared memorym
+int shm_id;
+
+//log filename
+char filename[MAX_STRING_SIZE];
+
+#endif
