1dc307f4c40de39bc61160ca32c880c456c09b44
Álvaro Terroso <106986189+alvaroterroso@users.noreply.github.com>
2024-03-21 12:32:23 +0000

    system_manager.h updated

--- system_manager.h
+++ system_manager.h
@@ -1,8 +1,15 @@
+#ifndef system_manager
+#define system_manager
+
 #include "shared_mem.h"
 
 bool validate_config(char * filename);
 
 void init_prog();
 
-//save the file content 
-config_struct config;
+void *sender_function(void *arg);
+
+void *receiver_function(void *arg);
+
+
+#endif
