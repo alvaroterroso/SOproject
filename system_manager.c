8eb9eb175858a6d9a0d1084a20014ba6f9c96557
RuiOliv4444 <132606623+RuiOliv4444@users.noreply.github.com>
2024-03-27 01:42:23 +0000

    Update system_manager.c

--- system_manager.c
+++ system_manager.c
@@ -5,9 +5,11 @@
 //CRIAR PROCESSOS ALERT WATCHER
 //CRIAR THREADS
 #include "system_manager.h"
+#include "log.h"
+#include <stdio.h>
 
 int main(int argc, char **argv){
-	printf("System starting...\n");
+	log_message("Program starting...\n");
 	if (argc < 2) {
         fprintf(stderr, "Usage: %s <config-file-path>\n", argv[0]);
         return 1;
@@ -17,6 +19,7 @@ int main(int argc, char **argv){
 	printf("%s read\n", filename);
 	init_prog();
 	free_shared(shm_id);
+	log_message("Program ending...");
 	return 0;
 }
 
@@ -29,36 +32,37 @@ void init_prog(){
 	//size incomplete
 	int shm_size = sizeof(config_struct) + sizeof(mobile_user_struct);
 	if ((shm_id = shmget(IPC_PRIVATE, shm_size, IPC_CREAT | IPC_EXCL | 0700)) < 1){
-    	printf("ERROR IN SHMGET\n");
+    	log_message("ERROR IN SHMGET\n");
     	exit(1);
   	}
 	if(shmat(shm_id, NULL, 0) == (void*)-1){
-		printf("ERROR IN SHMAT\n");
+		log_message("ERROR IN SHMAT\n");
 		exit(0);
 	}
+	log_message("Shared memory allocated.");
 	// Create threads
 	if (pthread_create(&sender_thread, NULL, sender_function, NULL) != 0)
 	{
-		printf("CANNOT CREATE SENDER_THREAD\n");
+		log_message("Cannot create sender_thread\n");
 		free_shared(shm_id);
 		exit(1);
 	}
 
 	if (pthread_create(&receiver_thread, NULL, receiver_function, NULL) != 0)
 	{
-		printf("CANNOT CREATE RECEIVER_THREAD\n");
+		log_message("Cannot create receiver_thread.\n");
 		free_shared(shm_id);
 		exit(1);
 	}
 
 	if(pthread_join(sender_thread, NULL)!= 0){
-		printf("CANNOT JOIN SENDER_THREAD\n");
+		log_message("Cannot join sender_thread.\n");
 		free_shared(shm_id);
 		exit(1);
 	}
 
 	if(pthread_join(receiver_thread, NULL)!= 0){
-		printf("CANNOT JOIN RECEIVER_THREAD\n");
+		log_message("Cannot join receiveR_thread\n");
 		free_shared(shm_id);
 		exit(1);
 	}
@@ -66,10 +70,10 @@ void init_prog(){
 }
 
 void auth_request_manager(){
-	printf("authorization Request Manager process created...\n");
+	log_message("authorization Request Manager process created...\n");
 }
 void monitor_engine(){
-	printf("monitor Engine processs created...\n");
+	log_message("monitor Engine processs created...\n");
 } 
 
 void create_proc(){
@@ -79,8 +83,9 @@ void create_proc(){
 		return;
 	}
 	else if(auth_request_manager_pid < 0){
-		perror("auth_request_manager fork failed");
+		log_message("auth_request_manager fork failed");
 		free_shared(shm_id);
+		exit(1);
 	}
 	monitor_engine_pid = fork();
 	if(monitor_engine_pid == 0){
@@ -88,61 +93,79 @@ void create_proc(){
 		return;
 	}
 	else if(monitor_engine_pid < 0){
-		perror("monitor_engine fork failed");
+		log_message("monitor_engine fork failed");
 		free_shared(shm_id);
+		exit(1);
 	}
-	wait(NULL);
+	// Esperar pela terminação dos processos filho para evitar processos zumbi
+    waitpid(auth_request_manager_pid, NULL, 0);
+    waitpid(monitor_engine_pid, NULL, 0);
+    log_message("Child processes have terminated.");
 }
 
 void *sender_function(void *arg){
 	(void)arg;
-	printf("sender thread functional... \n");
+	log_message("Sender thread functional... \n");
 	return NULL;
 }
 
 void *receiver_function(void *arg){
 	(void)arg;
-	printf("receiver thread functional...\n");
+	log_message("Receiver thread functional...\n");
 	return NULL;
 }
 
+
 bool validate_config(char *filename) {
     FILE *f = fopen(filename, "r");
+    char log_msg[256]; // Define a variável aqui, garantindo que seja grande o suficiente para a mensagem.
+
     if (f == NULL) {
-        perror("Unable to open the file");
+        log_message("Unable to open the config file.");
         return false;
     }
     int aux;
-    if (fscanf(f, "%d", &aux) < 1 || aux < 0) { // QUEUE_POS
-        printf("Error reading %s: %d must be >= 0\n", filename, aux);
+
+    // Lendo QUEUE_POS
+    if (fscanf(f, "%d", &aux) < 1 || aux < 0) {
+        snprintf(log_msg, sizeof(log_msg), "Error reading %s: QUEUE_POS %d must be >= 0", filename, aux);
+        log_message(log_msg);
         fclose(f);
         return false;
     }
     config.queue_slot_number = aux;
 
-    if (fscanf(f, "%d", &aux) < 1 || aux < 1) { // AUTH_SERVERS_MAX
-        printf("Error reading %s: %d must be >= 1\n", filename, aux);
+    // Lendo AUTH_SERVERS_MAX
+    if (fscanf(f, "%d", &aux) < 1 || aux < 1) {
+        snprintf(log_msg, sizeof(log_msg), "Error reading %s: AUTH_SERVERS_MAX %d must be >= 1", filename, aux);
+        log_message(log_msg);
         fclose(f);
         return false;
     }
     config.max_auth_servers = aux;
 
-    if (fscanf(f, "%d", &aux) < 1 || aux < 0) { // AUTH_PROC_TIME
-        printf("Error reading %s: %d must be >= 0\n", filename, aux);
+    // Lendo AUTH_PROC_TIME
+    if (fscanf(f, "%d", &aux) < 1 || aux < 0) {
+        snprintf(log_msg, sizeof(log_msg), "Error reading %s: AUTH_PROC_TIME %d must be >= 0", filename, aux);
+        log_message(log_msg);
         fclose(f);
         return false;
     }
     config.auth_proc_time = aux;
 
-    if (fscanf(f, "%d", &aux) < 1 || aux < 1) { // MAX_VIDEO_WAIT
-        printf("Error reading %s: %d must be >= 1\n", filename, aux);
+    // Lendo MAX_VIDEO_WAIT
+    if (fscanf(f, "%d", &aux) < 1 || aux < 1) {
+        snprintf(log_msg, sizeof(log_msg), "Error reading %s: MAX_VIDEO_WAIT %d must be >= 1", filename, aux);
+        log_message(log_msg);
         fclose(f);
         return false;
     }
     config.max_video_wait = aux;
 
-    if (fscanf(f, "%d", &aux) < 1 || aux < 1) { // MAX_OTHERS_WAIT
-        printf("Error reading %s: %d must be >= 1\n", filename, aux);
+    // Lendo MAX_OTHERS_WAIT
+    if (fscanf(f, "%d", &aux) < 1 || aux < 1) {
+        snprintf(log_msg, sizeof(log_msg), "Error reading %s: MAX_OTHERS_WAIT %d must be >= 1", filename, aux);
+        log_message(log_msg);
         fclose(f);
         return false;
     }
